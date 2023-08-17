d1800658bdac1380b0202bf9740b7fad8dfa1534
devferx <ferkki@hotmail.com>
2023-08-16 22:01:39 -0400

    Add word once per second

--- 01-fade-in-text/app.js
+++ 01-fade-in-text/app.js
@@ -1,10 +1,19 @@
 const words = "View Transitions API".toUpperCase().split(" ");
 const rate = 1000;
 const $message = document.querySelector("#container h1");
+const $container = document.querySelector("#container");
 let index = 0;
 
 setInterval(() => {
-  $message.textContent = words[index++];
+  const $newElement = document.createElement("h1");
+  $newElement.textContent = words[index++];
+  $newElement.classList.add("fade-in");
+  $newElement.addEventListener("animationend", function () {
+    this.classList.remove("fade-in");
+  });
+
+  $container.prepend($newElement);
+
   if (index >= words.length) {
     index = 0;
   }
